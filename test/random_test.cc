d6abe33ef0fdcea2b980a7f7381b833806f0c241
Pavel Komlev <komlev@gmail.com>
2023-08-15 16:33:06 -0600

    use tests from folly itself

--- test/random_test.cc
+++
@@ -1,9 +0,0 @@
-#include "folly/Random.h"
-
-#include "gtest/gtest.h"
-
-TEST(RandomTest, TestFollyRand32) {
-  uint32_t kMaxNum = 5;
-  uint32_t val = folly::Random::secureRand32(kMaxNum);
-  EXPECT_GT(kMaxNum, val);
-}
